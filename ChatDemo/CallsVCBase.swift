e1767ee242076c23f95950fb2271f6d5b6e44377
mahmudulshuvo <shuvo0201@gmail.com>
2016-10-05 14:35:45 +0600

    Buttons and structure are set for calls view

--- ChatDemo/CallsVCBase.swift
+++ ChatDemo/CallsVCBase.swift
@@ -15,98 +15,102 @@ class CallsVCBase: UIViewController {
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var plusBtn: UIButton!
     
-    @IBOutlet weak var call: UIImageView!
-    @IBOutlet weak var speaker: UIImageView!
-    @IBOutlet weak var video: UIImageView!
-    @IBOutlet weak var mute: UIImageView!
+    @IBOutlet weak var topStack: UIStackView!
+    @IBOutlet weak var displayStack: UIStackView!
+    @IBOutlet weak var middleStack: UIStackView!
+    @IBOutlet weak var bottomStack: UIStackView!
     
-
+    @IBOutlet weak var middleBtnStack: UIStackView!
+    @IBOutlet weak var callEndBtn: UIButton!
     @IBOutlet weak var displayView: UIView!
+    
     var circleView = UIView()
     var diameter: CGFloat = 0.0
     
-    var screenSize: CGSize = CGSize(width: 20, height: 30)
-    var counter:Int = 0
-    
-    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        setupConstraints()
         
- //       setupConstraints()
-        
-    }
-    
-    @IBAction func drawCircle(_ sender: AnyObject) {
-        
-        if counter == 0 {
-            counter += 1
-            diameter = min(self.displayView.frame.size.width, self.displayView.frame.size.height) * 0.6;
-            circleView.frame = CGRect(x :0,
-                                      y :0,
-                                      width :diameter, height :diameter);
-            circleView.layer.cornerRadius = diameter / 2;
-            circleView.clipsToBounds = true
-            circleView.layer.borderColor = UIColor.blue.cgColor
-            circleView.layer.borderWidth = 1.0
-            self.displayView.addSubview(circleView)
-            
-            circleView.translatesAutoresizingMaskIntoConstraints = false
-            circleView.centerXAnchor.constraint(equalTo: displayView.centerXAnchor).isActive = true
-            circleView.centerYAnchor.constraint(equalTo: displayView.centerYAnchor).isActive = true
-            circleView.widthAnchor.constraint(equalToConstant: diameter).isActive = true
-            circleView.heightAnchor.constraint(equalToConstant: diameter).isActive = true
-        }
-
     }
     
     
     func setupConstraints() {
         
-//        print("called \(-(screenSize.height * 0.5))")
-//        containerView.translatesAutoresizingMaskIntoConstraints = false
-//        containerView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
-//        containerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
-//        containerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
-//        containerView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-//        
-//        topLabel.translatesAutoresizingMaskIntoConstraints = false
-//        topLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
-//        topLabel.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
-//        
-//        plusBtn.translatesAutoresizingMaskIntoConstraints = false
-//        plusBtn.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
-//        plusBtn.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
-//        plusBtn.widthAnchor.constraint(equalToConstant: 45).isActive = true
-//        plusBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        topStack.translatesAutoresizingMaskIntoConstraints = false
+        topStack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        topStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        topStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+        topStack.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1, constant: 0).isActive = true
+
+        topLabel.translatesAutoresizingMaskIntoConstraints = false
+        topLabel.centerXAnchor.constraint(equalTo: topStack.centerXAnchor).isActive = true
+        topLabel.centerYAnchor.constraint(equalTo: topStack.centerYAnchor).isActive = true
         
-//        mute.translatesAutoresizingMaskIntoConstraints = false
-//        mute.bottomAnchor.constraint(equalTo: self.view.readableContentGuide.bottomAnchor, constant: -200).isActive = true
-//        mute.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:20).isActive = true
-//        mute.widthAnchor.constraint(equalToConstant: 80).isActive = true
-//        mute.heightAnchor.constraint(equalToConstant: 70).isActive = true
-//      //mute.heightAnchor.constraint(equalTo: , multiplier: , constant: )
-//        
-//        video.translatesAutoresizingMaskIntoConstraints = false
-//        video.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant:-200).isActive = true
-//        video.leadingAnchor.constraint(equalTo: mute.trailingAnchor, constant:55).isActive = true
-//        video.widthAnchor.constraint(equalToConstant: 80).isActive = true
-//        video.heightAnchor.constraint(equalToConstant: 70).isActive = true
-//        
-//        speaker.translatesAutoresizingMaskIntoConstraints = false
-//        speaker.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant:-200).isActive = true
-//        speaker.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant:-20).isActive = true
-//        speaker.widthAnchor.constraint(equalToConstant: 80).isActive = true
-//        speaker.heightAnchor.constraint(equalToConstant: 70).isActive = true
-//        
-//        call.translatesAutoresizingMaskIntoConstraints = false
-//        call.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-//        call.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant:-100).isActive = true
-//        call.widthAnchor.constraint(equalToConstant: 80).isActive = true
-//        call.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        plusBtn.translatesAutoresizingMaskIntoConstraints = false
+        plusBtn.centerYAnchor.constraint(equalTo: topStack.centerYAnchor).isActive = true
+        plusBtn.trailingAnchor.constraint(equalTo: topStack.trailingAnchor, constant: -10).isActive = true
+        plusBtn.widthAnchor.constraint(equalToConstant: 45).isActive = true
+        plusBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        displayStack.translatesAutoresizingMaskIntoConstraints = false
+        displayStack.topAnchor.constraint(equalTo: topStack.bottomAnchor, constant: 0).isActive = true
+        displayStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        displayStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+        displayStack.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.6, constant: -50).isActive = true
+        
+        displayView.translatesAutoresizingMaskIntoConstraints = false
+        displayView.topAnchor.constraint(equalTo: displayStack.topAnchor, constant: 0).isActive = true
+        displayView.leadingAnchor.constraint(equalTo: displayStack.leadingAnchor, constant: 0).isActive = true
+        displayView.trailingAnchor.constraint(equalTo: displayStack.trailingAnchor, constant: 0).isActive = true
+        displayView.bottomAnchor.constraint(equalTo: displayStack.bottomAnchor, constant: 0).isActive = true
+        
+        middleStack.translatesAutoresizingMaskIntoConstraints = false
+        middleStack.topAnchor.constraint(equalTo: displayStack.bottomAnchor, constant: 0).isActive = true
+        middleStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        middleStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+        middleStack.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.15, constant: 0).isActive = true
+        
+        middleBtnStack.translatesAutoresizingMaskIntoConstraints = false
+        middleBtnStack.topAnchor.constraint(equalTo: middleStack.topAnchor, constant: 0).isActive = true
+        middleBtnStack.leadingAnchor.constraint(equalTo: middleStack.leadingAnchor, constant: 30).isActive = true
+        middleBtnStack.trailingAnchor.constraint(equalTo: middleStack.trailingAnchor, constant: -30).isActive = true
+        middleBtnStack.bottomAnchor.constraint(equalTo: middleStack.bottomAnchor, constant: 0).isActive = true
+        middleStack.distribution = UIStackViewDistribution.fillEqually
+        middleStack.alignment = UIStackViewAlignment.fill
+        
+        bottomStack.translatesAutoresizingMaskIntoConstraints = false
+        bottomStack.topAnchor.constraint(equalTo: middleStack.bottomAnchor, constant: 0).isActive = true
+        bottomStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        bottomStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+        bottomStack.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -48).isActive = true
+        
+        callEndBtn.translatesAutoresizingMaskIntoConstraints = false
+        callEndBtn.centerXAnchor.constraint(equalTo: bottomStack.centerXAnchor).isActive = true
+        callEndBtn.centerYAnchor.constraint(equalTo: bottomStack.centerYAnchor).isActive = true
         
     }
 
     
+    @IBAction func drawCircle(_ sender: AnyObject) {
+        
+        diameter = min(self.displayView.frame.size.width, self.displayView.frame.size.height) * 0.6;
+        circleView.frame = CGRect(x :100,
+                                     y :100,
+                                     width :diameter, height :diameter);
+        circleView.layer.cornerRadius = diameter / 2;
+        circleView.clipsToBounds = true
+        circleView.layer.borderColor = UIColor.blue.cgColor
+        circleView.layer.borderWidth = 1.0
+        displayView.addSubview(circleView)
+      //  displayStack.addArrangedSubview(displayView)
+        
+        circleView.centerXAnchor.constraint(equalTo: displayView.centerXAnchor, constant: 0).isActive = true
+        circleView.centerYAnchor.constraint(equalTo: displayView.centerYAnchor, constant: 0).isActive = true
+        circleView.widthAnchor.constraint(equalTo: displayView.widthAnchor, multiplier: 0.25, constant: diameter).isActive = true
+        circleView.heightAnchor.constraint(equalTo: displayView.heightAnchor, multiplier: 0.25, constant: diameter).isActive = true
+
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
