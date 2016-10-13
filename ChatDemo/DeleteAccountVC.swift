870075d75a54ed979dad82066a06b10c959fe2f3
mahmudulshuvo <shuvo0201@gmail.com>
2016-10-13 21:17:37 +0600

    DeleteAccountVC.swift was missing on previous update

---
+++ ChatDemo/DeleteAccountVC.swift
@@ -0,0 +1,18 @@
+//
+//  AccountsVC.swift
+//  ChatDemo
+//
+//  Created by SHUVO on 10/12/16.
+//  Copyright © 2016 SHUVO. All rights reserved.
+//
+
+import UIKit
+
+class DeleteAccountVC: DeleteAccountVCBase {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // printDemo()
+    }
+    
+}
